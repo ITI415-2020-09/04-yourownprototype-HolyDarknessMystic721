6f6a8fb610ea69c2857d95072934d486c51e518d
HolyDarknessMystic721 <70783250+HolyDarknessMystic721@users.noreply.github.com>
2020-10-12 20:24:22 -0400

    WebGL Build Added and changed camera to top down view
    (No time to check 3d camera using cinemachine) might check it later

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,22 +8,26 @@ public class CameraController : MonoBehaviour
 
     private Vector3 offset;
 
-    private PlayerController    pc;
+    //private PlayerController    pc;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
 
-        pc = player.GetComponent<PlayerController>();
+       // pc = player.GetComponent<PlayerController>();
 
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position - pc.currentDirection * offset.magnitude;
-        this.gameObject.transform.LookAt(player.transform);
+        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position - pc.currentDirection * offset.magnitude;
+        //this.gameObject.transform.LookAt(player.transform);
 
     }
 }
+// Note: commented out sections were attempts to make a 3rd person camera follow the player.
+// set the rotation on Main camera to be X:180, Y:90, Z:180
+// set postion of main camera to X:0, Y:52, Z:0
